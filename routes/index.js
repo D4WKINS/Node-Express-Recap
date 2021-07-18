d5ba1a749fb984ac33f1659e9c0282276ea8e24d
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-07-18 03:46:49 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ import express from 'express';
 
 const router = express.Router();// allows us to use router in our server.js
 
-router.get('/',(req,res,next)=>{ // '/' is our default route 
+router.get('/',(req, res, next)=>{ // '/' is our default route 
 //This callback is required to define our get method, this is executed whenever a request is made to the root of our site
-    res.send('Hello World')// res.send as the name suggests sends data to the client/browser
+    res.send('Got nothing')// res.send as the name suggests sends data to the client/browser
+
 })
+
+router.get('/hello',(req, res, next)=>{ // '/' is our default route 
+    //This callback is required to define our get method, this is executed whenever a request is made to the root of our site
+        res.send('<h1>Hello World</h1>')// res.send as the name suggests sends data to the client/browser
+    
+    })
 
 export default router
